f35add12af51d2a0f5187196685037f2dba1e559
BuildTools <unconfigured@null.spigotmc.org>
2017-09-13 00:01:41 +0800

    Main class JavaDoc Test

--- src/tw/yeemo/YeemoGameAPI.java
+++ src/tw/yeemo/YeemoGameAPI.java
@@ -2,6 +2,13 @@ package tw.yeemo;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * -=-=-= YeemoGameLib =-=-=-
+ * @author WenWen
+ * @version 0.1 - (20170912)
+ * @deprecated YeemoGameLib main class
+ *
+ */
 public class YeemoGameAPI extends JavaPlugin {
 
     @Override
