6a88ab61d825ae85131c598f011a53b57489fa2e
BuildTools <unconfigured@null.spigotmc.org>
2017-09-12 23:37:42 +0800

    First Push HelloWorld

---
+++ src/tw/yeemo/YeemoGameAPI.java
@@ -0,0 +1,11 @@
+package tw.yeemo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class YeemoGameAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("HelloWorld!");
+    }
+}
